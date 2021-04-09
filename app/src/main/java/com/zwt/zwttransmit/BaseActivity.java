7bd0d16340401fff5e0d157ce2cb79750b414b4a
moleinuoyi <2598583973@qq.com>
2021-04-09 20:31:09 +0800

    添加 BaseActivity 添加 ActivityCollector activity管理类

---
+++ app/src/main/java/com/zwt/zwttransmit/BaseActivity.java
@@ -0,0 +1,24 @@
+package com.zwt.zwttransmit;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+    private static final String TAG = "BaseActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, getClass().getSimpleName());
+        ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+}
--- app/src/main/java/com/zwt/zwttransmit/MainActivity.java
+++ app/src/main/java/com/zwt/zwttransmit/MainActivity.java
@@ -1,10 +1,8 @@
 package com.zwt.zwttransmit;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
